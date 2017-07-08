63124d6cc2745d16a854d5cbd64e99cbf96c0fba
SeungBum Kim <picxenk@gmail.com>
2017-07-09 01:24:47 +0900

    init kiosk page

---
+++ public_html/kiosk.js
@@ -0,0 +1,12 @@
+var setup = function() {
+    createCanvas(displayWidth, displayHeight);
+    noLoop();
+    background(0, 0, 200);
+    stroke(200, 0, 0);
+    line(0, 0, displayWidth, displayHeight);
+    line(displayWidth, 0, 0, displayHeight);
+}
+
+var draw = function() {
+}
+
