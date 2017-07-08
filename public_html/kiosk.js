3cd4869fcbeb2323143f9951d13a0bdda127532d
SeungBum Kim <picxenk@gmail.com>
2017-07-09 01:26:13 +0900

    no cursor on kiosk

--- public_html/kiosk.js
+++ public_html/kiosk.js
@@ -1,6 +1,7 @@
 var setup = function() {
     createCanvas(displayWidth, displayHeight);
     noLoop();
+    noCursor();
     background(0, 0, 200);
     stroke(200, 0, 0);
     line(0, 0, displayWidth, displayHeight);
