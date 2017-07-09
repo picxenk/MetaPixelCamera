9f4db1371c0177d5c3dda55c2965edcfeaa2b3ab
SeungBum Kim <picxenk@gmail.com>
2017-07-09 11:25:01 +0900

    scale down for speed

--- public_html/test_phantom.js
+++ public_html/test_phantom.js
@@ -4,7 +4,7 @@ page.viewportSize = { width: 960, height: 540 };
 //the clipRect is the portion of the page you are taking a screenshot of
 page.clipRect = { top: 0, left: 0, width: 960, height: 540 };
 //the rest of the code is the same as the previous example
-page.open('http://localhost/renderMP.html?img=20170709-03_59_29570.jpg', function() {
+page.open('http://localhost/renderMP.html?', function() {
   page.render('img/0_test_phantom.png');
   phantom.exit();
 });
